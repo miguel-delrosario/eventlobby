eec616dd929196c1816e11273a69b83064984a4a
miguel-dr <migdelrosario@gmail.com>
2016-10-29 16:19:59 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -5,11 +5,12 @@ angular.module('portalApp')
 	$scope.items = [
 		{
 			title:'Math 135 Study Group',
-			tags: ['Study Group', 'Math 135', 'QNC'],
+			tags: ['Study Group', 'Math 135', 'M3'],
             details: 'A study group for Math 135 A06',
             location: 'M3 1234',
             time: 'October 30, 5pm - 7pm',
-            capacity: '2/4'
+            capacity: '2/4',
+            addedby: 'fhamdullahpur'
 		},
 		{
 			title:'Hug a Goose, Break a Record',
@@ -17,15 +18,35 @@ angular.module('portalApp')
             details: 'Waterloo is attempting the world record for the most hugs given to a large stuffed toy in an 8 HOUR ACTION PACKED GOOSE HUGGING MARATHON!!!',
             location: 'Campus-Wide',
             time: 'November 4, 11am - 7pm',
-            capacity: 'Unlimited'
+            capacity: 'Unlimited',
+            addedby: 'mgoose'
 		},
         {
-			title:'Mass Migration 2k16',
-			tags: ['Migration', 'Geese', 'Too cold in loo'],
-            details: 'The annual mass migration event led by Mr. Goose.',
-            location: 'Anywhere but here',
-            time: 'October-April',
-            capacity: '>=0 geese'
+			title:'Haunted Hallways',
+			tags: ['Miscellaneous', 'V1', 'Halloween'],
+            details: 'A pseudo haunted house put on by the E1 Quad of V1.',
+            location: 'V1 E1 Quad',
+            time: 'October 27, 8pm - 11pm',
+            capacity: '3/6',
+            addedby: 'mpanino'
+		},
+        {
+			title:'CS 135 Study Group',
+			tags: ['Study Group', 'CS 135', 'QNC'],
+            details: 'A study group for CS 135 Mid Term 2',
+            location: 'QNC 1234',
+            time: 'October 31, 4pm - 7pm',
+            capacity: '3/6',
+            addedby: 'mpanino'
+		},
+        {
+			title:'Humans vs. Zombies',
+			tags: ['Miscellaneous', 'Campus-Wide', 'Halloween'],
+            details: 'WatSFiC is proud to present Humans versus Zombies at the University of Waterloo. It is essentially a giant game of tag consisting of two teams, one steadily losing members that join the other side. Humans win if they survive the week and complete their final mission whereas the zombies win if they thwart the humans.',
+            location: 'Campus-Wide',
+            time: 'October 31 - November 4',
+            capacity: 'Unlimited',
+            addedby: 'Sci-Fi Club'
 		}
 	];
 	
